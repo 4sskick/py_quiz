b073363a1b43ad283ea13e1727f471df1d2f8aba
Septian Adi Wijaya <developadi@gmail.com>
2022-03-13 23:20:51 +0700

    convert into tuple & present question on start

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import json
 import os
+import random
+from collections import namedtuple
+
+question_data = namedtuple('question_data', 'question, answers, answer_key')
 
 
 class QuizApp(object):
@@ -7,6 +11,8 @@ class QuizApp(object):
         self.chapter = ""
         self.title = ""
         self.no = ""
+
+        self.Data = None
 
         # check for file name
         if fileName != None:
@@ -25,6 +31,37 @@ class QuizApp(object):
             self.title = data_file['title']
             self.no = data_file['no']
 
+            # data = data_file['data']
+
+            # print(type(data))
+            # print(f"size: {len(data)} - {data[0]['answers'][0]}")
+            #
+            # for data_item in data:
+            #     self.question_data.append(data_item['question'])
+            #     print(f"{self.question_data}\n{type(data_item['answers'])}")
+
+            self.Data = [question_data(**ind) for ind in data_file['data']]
+
+            # print(f"{self.Data}\nover all data: {len(self.Data)}\n{len(self.Data[0].answers)}")
+            # print(f"chapter: {self.chapter}, title: {self.title}, no: {self.no}")
+
+            # print(random.sample(self.Data, 1))
+
+    def start(self, num_question_to_display):
+        '''
+        begin the quiz
+        question gonna be in random pick
+        '''
+
+        print(f"Evaluasi {self.title} - {self.chapter} ke {self.no}")
+        input("Tekan {ENTER} apabila Anda sudah siap >>>")
+
+        random_data = random.sample(self.Data, num_question_to_display)
+
+        for i in range(len(random_data)):
+            print(f"[Soal: {i + 1}]")
+            print(f"{random_data[i].question}")
+
 
 # https://stackoverflow.com/questions/9271464/what-does-the-file-variable-mean-do
-QuizApp(os.path.dirname(__file__) + '/question.json')
+QuizApp(os.path.dirname(__file__) + '/question.json').start(3)
