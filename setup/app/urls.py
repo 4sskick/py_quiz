2e4510a7ca29b28570831a691612f2386f70b5f3
Septian Adi Wijaya <developadi@gmail.com>
2022-04-15 22:36:38 +0700

    page - questions

--- setup/app/urls.py
+++ setup/app/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views as appViews
 
 urlpatterns = [
-    url(r'^$', appViews.index)
+    url(r'^$', appViews.index),
+    url(r'^questions/', appViews.quiz)
 ]
--- setup/app/views.py
+++ setup/app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Quiz
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def quiz(request):
+    questions = Quiz.objects.all()
+
+    return render(request, 'quiz.html', {'questions': questions})
+
+
+def admin(request):
+    return render(request, 'admin.html')
