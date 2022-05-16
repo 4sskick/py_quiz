043ac5aac97b58ef60bd14b19d3371a4eef8d402
Septian Adi Wijaya <developadi@gmail.com>
2022-05-16 21:33:32 +0700

    admin page - done

--- setup/app/views.py
+++ setup/app/views.py
@@ -9,12 +9,42 @@ from django.shortcuts import render
 from .models import Quiz
 
 
+def __getAnswerByIndex(j, item):
+    return {
+        "content": item['answers'][j],
+        "is_answer": int(j == item['answer_key'])
+    }
+
+
 def __load_questions():
     question_data = namedtuple('question_data', 'question, answers, answer_key')
-    with open(os.path.dirname(__file__)+'/data/question.json', 'r') as file:
+    questions = []
+
+    with open(os.path.dirname(__file__) + '/data/question.json', 'r') as file:
         quiz_json = json.load(file)
 
-        questions = [question_data(**ind) for ind in quiz_json['data']]
+        # questions = [question_data(**ind) for ind in quiz_json['data']]
+
+        for i in range(len(quiz_json)):
+            item = quiz_json['data'][i]
+
+            answerKey = list(map(
+                # __getAnswerByIndex(_, item['answers'])
+
+                # function to handle iterable
+                lambda index_iterable: {
+                    "content": item['answers'][index_iterable],
+                    "is_answer": int(index_iterable == item['answer_key'])
+                }
+
+                # loop iterate from index 0 ... length of item['answers]
+                , range(len(item['answers']))
+            ))
+
+            questions.append({
+                "question": item['question'],
+                "readable_answer_key": answerKey
+            })
 
     return questions
 
